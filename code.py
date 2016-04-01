279fd1ec876deaa5add1e2f270e5971b16499d94
tianyu1991 <hulan1991@gmail.com>
2016-04-01 00:52:36 -0400

    Update code.py

--- code.py
+++ code.py
@@ -40,3 +40,25 @@ while sum%10==0:
     sum=sum//10
     N2+=1
 print N2
+
+#Recently you invented a brand-new definition of prime numbers. For a given set of positive integers S let's call X a prime if there are no elements in S which are divisors of X (except X itself).
+def fun(li):
+    li2=[]
+    for i in li:
+        p=0
+        for n in li:
+            if (i%n==0) and (i!=n):
+                p=1
+        if p==0:
+            li2.append(str(i))
+    return li2
+
+n=raw_input()
+li=[int(n, 10) for n in raw_input().split(" ")]
+
+if n==0:
+    li2=li
+else:
+    li2=fun(li)
+
+print(" ".join(li2))
